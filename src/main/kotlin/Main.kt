0264f7480e2a2763115db5a6d2e507b4de295aa4
Alex Sánchez <alex.sanchez@streamsets.com>
2022-12-12 20:14:41 +0100

    Day05 Part2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
-import day05.Day05Part1
+import day05.Day05Part2
 
 //This is a simple main to be able to call different problems, it is not meant to anything else.
 
 fun main() {
-  val adventOfCode = Day05Part1() //Update this line with the
+  val adventOfCode = Day05Part2() //Update this line with the
   println(adventOfCode.runProblem())
 }
---
+++ src/main/kotlin/day05/Day05Part2.kt
@@ -0,0 +1,61 @@
+package day05
+
+import AdventDayProblem
+import java.io.File
+import java.util.*
+import kotlin.collections.ArrayList
+
+class Day05Part2 : AdventDayProblem {
+  private val inputFile = "src/main/kotlin/day05/day05.txt"
+  private val cratesPosition = HashMap<Int, Stack<String>>().toMutableMap()
+  override fun runProblem(): String {
+    val listOfTops = ArrayList<String>()
+    val crates = File(inputFile).readText().split("\n\n")[0].split("\n").reversed().toMutableList()
+    val moves = File(inputFile).readText().split("\n\n")[1].split("\n")
+
+    parseCrates(crates)
+    moveCrates(moves)
+
+    for (tops in cratesPosition.values) {
+      listOfTops.add(tops.peek())
+    }
+    return listOfTops.toString().replace(", ", "")
+  }
+
+  private fun moveCrates(moves: List<String>) {
+    for (move in moves) {
+      var numberOfMoves = move.split(" ")[1].toInt()
+      val originStack = move.split(" ")[3].toInt()
+      val destinationStack = move.split(" ")[5].toInt()
+
+      val tempArray = ArrayList<String>()
+
+      while (numberOfMoves != 0) {
+        tempArray.add(cratesPosition[originStack]!!.pop())
+        numberOfMoves--
+      }
+
+      cratesPosition[destinationStack]!!.addAll(tempArray.asReversed())
+    }
+  }
+
+  private fun parseCrates(crates: MutableList<String>) {
+    crates.removeAt(0)
+    for (crateStack in crates) {
+      val cratesList =
+        crateStack
+          .replace("[", "")
+          .replace("]", "")
+          .replace("    ", "|")
+          .replace(" ", "")
+      for ((index, crateInformation) in cratesList.split("").withIndex()) {
+        if (crateInformation.isNotBlank() && !crateInformation.contains("|")) {
+          if (cratesPosition[index] == null) {
+            cratesPosition[index] = Stack()
+          }
+          cratesPosition[index]?.add(crateInformation)
+        }
+      }
+    }
+  }
+}
