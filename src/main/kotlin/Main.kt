0d9b7786d9826e81464472cc15a5a6bd3c961a2e
Alex Sánchez <alex.sanchez@streamsets.com>
2022-12-13 10:25:01 +0100

    Day06 Part1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
-import day05.Day05Part2
+import day06.Day06Part1
 
 //This is a simple main to be able to call different problems, it is not meant to anything else.
 
 fun main() {
-  val adventOfCode = Day05Part2() //Update this line with the
+  val adventOfCode = Day06Part1() //Update this line with the
   println(adventOfCode.runProblem())
 }
---
+++ src/main/kotlin/day06/Day06Part1.kt
@@ -0,0 +1,23 @@
+package day06
+
+import AdventDayProblem
+import java.io.File
+
+class Day06Part1 : AdventDayProblem {
+  private val inputFile = "src/main/kotlin/day06/day06.txt"
+  override fun runProblem(): String {
+    val signal = File(inputFile).readText()
+
+    for (index in 4..signal.length) {
+      val set = HashSet<Char>()
+      for (element in signal.substring(index - 4, index)) {
+        set.add(element)
+      }
+      if (set.size == 4) {
+        return index.toString()
+      }
+    }
+
+    return signal
+  }
+}
